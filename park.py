c1b4f1f9d24473f7927e18ee50e3e7389808b7a0
Sharad Shriyan <sharad30.ss@gmail.com>
2022-11-13 15:10:06 +0530

    style: add appropriate print statements

--- park.py
+++ park.py
@@ -2,17 +2,22 @@ import json
 import random
 import string
 from pathlib import Path
-from typing import Union
+from typing import Union, List
 
 from parkingmanager import Car, ParkingLot, upload_file_to_s3
 from parkingmanager.park_utils import create_cars, random_park
 
 
-def cars_park(no_of_cars: int):
-    cars = create_cars(no_of_cars=no_of_cars)
+def cars_park(cars: List[Car]):
     parking_lot = ParkingLot(sq_footage=(200, 10))
-    _, not_parked_cars = random_park(cars=cars, parking_lot=parking_lot)
-    print(f"Cars not parked due to full parking: {not_parked_cars}")
+    car_parking_status, not_parked_cars = random_park(cars=cars, parking_lot=parking_lot)
+    print("----------------------------------------------------------------")
+    print(f"Parking size: {parking_lot.sq_footage} sq.ft")
+    print(f"Parking capacity: {parking_lot.parking_capacity}")
+    print("----------------------------------------------------------------")
+    print(f"Total cars to park: {len(cars)}")
+    print(f"Total cars parked: {car_parking_status.count('Car parked')}")
+    print(f"Total cars not parked due to full parking: {len(not_parked_cars)}")
     return parking_lot
 
 
@@ -25,9 +30,12 @@ def save_vehicle_parking_details(filepath: Union[Path, str], parking_lot: Parkin
 
 
 if __name__ == "__main__":
-    parking_lot = cars_park(no_of_cars=23)
+    cars = create_cars(no_of_cars=23)
+    parking_lot = cars_park(cars=cars)
+    print("----------------------------------------------------------------")
     root_dir = Path("output")
     filename = "parking_details.json"
     filepath = root_dir / filename
     save_vehicle_parking_details(filepath=filepath, parking_lot=parking_lot)
+    print("----------------------------------------------------------------")
     upload_file_to_s3(file_name=str(root_dir / filename), bucket="parking-manager")
