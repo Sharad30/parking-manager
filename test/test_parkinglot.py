1262cc565274c0281532f9a27cbd1fe38c313314
Sharad Shriyan <sharad30.ss@gmail.com>
2022-11-13 14:53:48 +0530

    test: add more parking lot tests

--- test/test_parkinglot.py
+++ test/test_parkinglot.py
@@ -11,3 +11,13 @@ def test_parking_spots_with_sq_footage_int():
 def test_parking_spots_with_sq_footage_tuple():
     parking_lot = ParkingLot(sq_footage=(200, 10))
     assert len(parking_lot.parking_spots) == 20
+
+
+def test_parking_spots_with_custom_parking_spot_size():
+    parking_lot = ParkingLot(sq_footage=(200, 10), parking_spot_size=(5, 5))
+    assert parking_lot.parking_capacity == 80
+
+
+def test_parking_spots_with_parking_spot_size_120ft():
+    parking_lot = ParkingLot(sq_footage=(200, 10), parking_spot_size=(10, 12))
+    assert parking_lot.parking_capacity == 16
