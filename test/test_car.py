3b47a479f6a70b6ba714acbcd312e4bf5ca545cd
Sharad Shriyan <sharad30.ss@gmail.com>
2022-11-13 14:53:10 +0530

    refactor: code abstraction

--- test/test_car.py
+++ test/test_car.py
@@ -3,14 +3,9 @@ from parkingmanager import Car
 
 def test_car_license_plate_property():
     car = Car(license_no="ABC-123")
-    assert car.license_plate == "ABC-123"
-
-
-def test_car_repr():
-    car = Car(license_no="ABC-123")
-    assert repr(car) == "ABC-123"
+    assert car.license_no == "ABC-123"
 
 
 def test_car_str():
     car = Car(license_no="ABC-123")
-    assert str(car) == "Car with license plate ABC-123"
+    assert str(car) == "ABC-123"
--- test/test_end_to_end.py
+++ test/test_end_to_end.py
@@ -1,59 +1,57 @@
 import random
 import string
-
+import pytest
 from parkingmanager import Car, ParkingLot
+from parkingmanager.park_utils import random_park, create_cars
 
 
-def test_parking_single():
-    car = Car(license_no="ABC-123")
+@pytest.fixture
+def cars_config():
+    cars_config = {}
+    for no_of_cars in [1, 3, 16, 23]:
+        cars = create_cars(no_of_cars=no_of_cars)
+        cars_config[f"car{no_of_cars}"] = cars
+    return cars_config
+
+
+def test_parking_single(cars_config):
+    car = cars_config["car1"][0]
     parking_lot = ParkingLot(sq_footage=(200, 10))
     spot_no = random.randint(0, parking_lot.parking_capacity - 1)
-    assert car.park(parking_lot, spot_no) == "Car parked"
+    assert car.park(parking_lot, spot_no)["status"] == "Car parked"
 
 
-def test_parking_multiple():
-    cars = []
-    no_of_cars = 3
-    for _ in range(0, no_of_cars):
-        random_license_no = "".join(
-            random.choices(string.ascii_uppercase + string.digits, k=7)
-        )
-        cars.append(Car(license_no=random_license_no))
+def test_parking_multiple(cars_config):
+    cars = cars_config["car3"]
     parking_lot = ParkingLot(sq_footage=(200, 10))
-    car_parking_status = []
-    for car in cars:
-        parking_status = "Car not parked"
-        while parking_status == "Car not parked":
-            spot_no = random.randint(0, parking_lot.parking_capacity - 1)
-            parking_status = car.park(parking_lot, spot_no)
-        car_parking_status.append(parking_status)
-    assert (
-        sum([1 if status == "Car parked" else 0 for status in car_parking_status]) == 3
-    )
+    car_parking_status, _ = random_park(cars=cars, parking_lot=parking_lot)
+    assert sum([1 if status == "Car parked" else 0 for status in car_parking_status]) == 3
 
 
-def test_parking_multiple_full():
-    cars = []
-    no_of_cars = 23
-    for _ in range(0, no_of_cars):
-        random_license_no = "".join(
-            random.choices(string.ascii_uppercase + string.digits, k=7)
-        )
-        cars.append(Car(license_no=random_license_no))
+def test_parking_multiple_full(cars_config):
+    cars = cars_config["car23"]
     parking_lot = ParkingLot(sq_footage=(200, 10))
-    car_parking_status = []
-    not_parked_cars = []
-    for car in cars:
-        parking_status = "Car not parked"
-        total_occupied_spots = sum(
-            [1 if status == "Car parked" else 0 for status in car_parking_status]
-        )
-        parking_full_condition = total_occupied_spots == parking_lot.parking_capacity
-        if not parking_full_condition:
-            while parking_status == "Car not parked":
-                spot_no = random.randint(0, parking_lot.parking_capacity - 1)
-                parking_status = car.park(parking_lot, spot_no)
-        else:
-            not_parked_cars.append(car)
-        car_parking_status.append(parking_status)
-    assert (total_occupied_spots == no_of_cars - 3) and (len(not_parked_cars) == 3)
+    car_parking_status, not_parked_cars = random_park(cars=cars, parking_lot=parking_lot)
+    assert (car_parking_status.count("Car parked") == 20) and (len(not_parked_cars) == 3)
+
+
+def test_negative_parking_spot_no(cars_config):
+    car = cars_config["car1"][0]
+    parking_lot = ParkingLot(sq_footage=(200, 10))
+    response = car.park(parking_lot, spot_no=-1)
+    assert response["status"] == "Car not parked" and response["error"] == "InvalidSpotError"
+
+
+def test_greater_than_available_parking_spot_no(cars_config):
+    car = cars_config["car1"][0]
+    parking_lot = ParkingLot(sq_footage=(200, 10))
+    response = car.park(parking_lot, spot_no=100)
+    assert response["status"] == "Car not parked" and response["error"] == "InvalidSpotError"
+
+
+def test_already_paarked_parking_spot_no(cars_config):
+    car = cars_config["car1"][0]
+    parking_lot = ParkingLot(sq_footage=(200, 10))
+    parking_lot.parking_spots[0] = 1
+    response = car.park(parking_lot, spot_no=0)
+    assert response["status"] == "Car not parked" and response["error"] == "SpotNotAvailableError"
